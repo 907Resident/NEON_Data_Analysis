7e9f3824a81530bda4e71fd1df82679df26f0fc8
Moyo Ajayi <moyo.ajayi@gmail.com>
2020-01-09 14:46:42 -0600

    Can grab all IRGA CO2 data at the ORNL tower

--- NEON_ORNL_Data/NEON_Data_Wrangler.py
+++ NEON_ORNL_Data/NEON_Data_Wrangler.py
@@ -14,7 +14,7 @@ import numpy  as np
 import pandas as pd
 import matplotlib.pyplot as plt
 # Path for HD5F file
-raw_file = r'ORNL_data/ORNL-unzipped-data/NEON.D07.ORNL.DP4.00200.001.nsae.2019-10-24.expanded.h5'
+raw_file = r'ORNL-unzipped-data/NEON.D07.ORNL.DP4.00200.001.nsae.2019-10-24.expanded.h5'
 # Open up HD5F file
 h5_file = h5py.File(raw_file, 'r')
 
@@ -27,7 +27,7 @@ txt = h5_file['readMe']
 rdMe = txt[0,]
 
 # Write out the readMe file to a text file
-with open(r'ORNL_data/rdMe.txt', 'wb') as w:
+with open(r'rdMe.txt', 'wb') as w:
     w.write(rdMe)
 
 #%% Organize pertinent data into dataframes
@@ -43,14 +43,32 @@ data.keys()
 dp0p            = data['dp0p']['data']
 dp0p.keys()
 ###### Gather the CO2 CRDS data
-dp0p['crdCO2']
-
+dp0p_crdCo2 = dp0p['crdCo2']
+####### ^^^ No data for this entry
 ### Level 1
 dp01p           = data['dp01']['data']
-###### Gather the CO2 profile data
+###### Gather the CO2 profile data -- IRGA [CO2]
 ######## Level 01 | 30-min resolution
 dp01p_lvl01_co2Stor = dp01p['co2Stor']['000_010_30m']['rtioMoleDryCo2']
 dp01p_lvl01_co2Stor = dp01p['co2Stor']['000_010_30m']['rtioMoleDryCo2'][:len(dp01p_lvl01_co2Stor)]
+######## Level 02 | 30-min resolution
+dp01p_lvl02_co2Stor = dp01p['co2Stor']['000_020_30m']['rtioMoleDryCo2']
+dp01p_lvl02_co2Stor = dp01p['co2Stor']['000_020_30m']['rtioMoleDryCo2'][:len(dp01p_lvl02_co2Stor)]
+######## Level 03 | 30-min resolution
+dp01p_lvl03_co2Stor = dp01p['co2Stor']['000_030_30m']['rtioMoleDryCo2']
+dp01p_lvl03_co2Stor = dp01p['co2Stor']['000_030_30m']['rtioMoleDryCo2'][:len(dp01p_lvl03_co2Stor)]
+######## Level 04 | 30-min resolution
+dp01p_lvl04_co2Stor = dp01p['co2Stor']['000_040_30m']['rtioMoleDryCo2']
+dp01p_lvl04_co2Stor = dp01p['co2Stor']['000_040_30m']['rtioMoleDryCo2'][:len(dp01p_lvl04_co2Stor)]
+######## Level 05 | 30-min resolution
+dp01p_lvl05_co2Stor = dp01p['co2Stor']['000_050_30m']['rtioMoleDryCo2']
+dp01p_lvl05_co2Stor = dp01p['co2Stor']['000_050_30m']['rtioMoleDryCo2'][:len(dp01p_lvl05_co2Stor)]
+######## Level 06 | 30-min resolution
+dp01p_lvl06_co2Stor = dp01p['co2Stor']['000_060_30m']['rtioMoleDryCo2']
+dp01p_lvl06_co2Stor = dp01p['co2Stor']['000_060_30m']['rtioMoleDryCo2'][:len(dp01p_lvl06_co2Stor)]
 
 
 # EC Tower CO2 Fluxes
+
+
+# %%
