25f5747056a8acc1a959552efeb32b6c2d2ccec4
Moyo Ajayi <moyo.ajayi@gmail.com>
2020-01-30 13:34:24 -0600

    minor changes to the script

--- ORNL/NEON_ORNL_Data_Wharehouse/NEON_Data_Compiler.py
+++ ORNL/NEON_ORNL_Data_Wharehouse/NEON_Data_Compiler.py
@@ -27,7 +27,8 @@ df_ORNL = df_ORNL.drop('faux_index', axis=1)
 
 #%% Exploratory Data Analysis
 
-# Multiple Levels of IRGA Tower at CO2
+# Multiple Levels with IRGA on NEON Tower -- CO2
+plt.style.use('seaborn-darkgrid')
 df_ORNL.plot(x='timeBgn', y=['IRGA_lvl01_Co2_mu', 'IRGA_lvl02_Co2_mu', 
             'IRGA_lvl03_Co2_mu', 'IRGA_lvl04_Co2_mu', 'IRGA_lvl05_Co2_mu',
             'IRGA_lvl06_Co2_mu'], 
@@ -35,8 +36,42 @@ df_ORNL.plot(x='timeBgn', y=['IRGA_lvl01_Co2_mu', 'IRGA_lvl02_Co2_mu',
 plt.ylabel("[CO2] (ppm)")
 plt.xlabel('')
 plt.xticks(rotation=45)
-plt.style.use('seaborn-darkgrid')
 plt.show()
 
-# 
-# %%
+# Multiple Levels with CRDS on NEON Tower -- CO2 
+plt.style.use('seaborn-darkgrid')
+df_ORNL.plot(x='timeBgn', y=['isoCO2_lvl01_dryCo2_mu', 'isoCO2_lvl02_dryCo2_mu', 
+            'isoCO2_lvl03_dryCo2_mu', 'isoCO2_lvl04_dryCo2_mu', 'isoCO2_lvl05_dryCo2_mu',
+            'isoCO2_lvl06_dryCo2_mu'], 
+             kind='line', title='CRDS CO2 Measurements',grid=True)
+plt.ylabel("[CO2] (ppm)")
+plt.xlabel('')
+plt.xticks(rotation=45)
+plt.show()
+
+# Multiple Levels with CRDS on NEON Tower -- d13C-CO2 
+plt.style.use('seaborn-darkgrid')
+df_ORNL.plot(x='timeBgn', y=['isoCO2_lvl01_iCo2_mu', 'isoCO2_lvl02_iCo2_mu', 
+            'isoCO2_lvl03_iCo2_mu', 'isoCO2_lvl04_iCo2_mu', 'isoCO2_lvl05_iCo2_mu',
+            'isoCO2_lvl06_iCo2_mu'], 
+             kind='line', title='CRDS δ13C-CO2 Measurements',grid=True)
+plt.ylabel("δ13C-CO2 (‰)")
+plt.xlabel('')
+plt.xticks(rotation=45)
+plt.show()
+
+# Subplots
+## IRGA-lvl05 | CRDS-lvl05  | d13CO2-lvl-05 | Temp-lvl05 | EC-NetFlux
+plt.style.use('seaborn-darkgrid')
+ax = df_ORNL.plot(x='timeBgn', y=['IRGA_lvl05_Co2_mu', 'isoCO2_lvl05_dryCo2_mu',
+            'isoCO2_lvl05_iCo2_mu', 'Lvl05_tempAir_mu', 'EC_co2FlxNSAE'], 
+            subplots=True, figsize=(8,8), title='NEON ORNL CO2 Tower Data')
+ax[0].set_ylabel('[CO2] (ppm)')
+ax[1].set_ylabel('[CO2] (ppm)')
+ax[2].set_ylabel('δ13C-CO2 (‰)')
+ax[3].set_ylabel('°C')
+ax[4].set_ylabel('mmol m-2 s-1')
+plt.legend(loc='best')
+plt.xlabel('')
+
+#%%
