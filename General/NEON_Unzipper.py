c92d2813e11d195de884e3aae203c096c2513c02
Moyo Ajayi <moyo.ajayi@gmail.com>
2020-01-30 15:20:31 -0600

    modified the script to unzip soil data

--- General/NEON_Unzipper.py
+++ General/NEON_Unzipper.py
@@ -4,15 +4,15 @@
 # author: Moyo Ajayi
 # 
 # description: [works with previously downloaded files; sec01-unzip files]
-# 
+# will need to turn this into a function [30Jan2020]
 
 
 #%% Unzip Downloaded files
 
 # Path of zipped files
-zipped_data_query    = r'NEON_eddy-flux.zip'
+zipped_data_query    = r'C:/Users/moyoa/Google Drive/CompSci/PhD Dissertation/Data Analysis/Picarro/Cumberland/Vandy_NOAA_OakRidge/NEON/ORNL_data/NEON_conc-co2-soil.zip'
 # Path to deposit unzipped files
-unzipped_data_query  = r'ORNL_data'
+unzipped_data_query  = r'C:/Users/moyoa/Google Drive/CompSci/PhD Dissertation/Data Analysis/Picarro/Cumberland/Vandy_NOAA_OakRidge/NEON/ORNL_data'
 # Import Necessary Packages
 import zipfile
 with zipfile.ZipFile(zipped_data_query, 'r') as zip_ref:
@@ -21,9 +21,9 @@ with zipfile.ZipFile(zipped_data_query, 'r') as zip_ref:
 #%% October 2019 Data
 
 # Path of zipped files
-zipped_Oct2019         = r'ORNL_data/NEON.D07.ORNL.DP4.00200.001.2019-10.expanded.20191202T191735Z.zip'
+zipped_Oct2019         = r'C:/Users/moyoa/Google Drive/CompSci/PhD Dissertation/Data Analysis/Picarro/Cumberland/Vandy_NOAA_OakRidge/NEON/ORNL_data/NEON.D07.ORNL.DP1.00095.001.2019-10.expanded.20191105T194150Z.zip'
 # Path to deposit unzipped files
-unzipped_data_Oct2019  = r'ORNL_data/ORNL-unzipped-data'
+unzipped_data_Oct2019  = r'C:/Users/moyoa/Google Drive/CompSci/PhD Dissertation/Data Analysis/Picarro/Cumberland/Vandy_NOAA_OakRidge/NEON/ORNL_data/ORNL-unzipped-data'
 
 with zipfile.ZipFile(zipped_Oct2019, 'r') as zip_ref:
     zip_ref.extractall(unzipped_data_Oct2019)
